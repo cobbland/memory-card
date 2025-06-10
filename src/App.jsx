c3719f83d1e4742e2c6088caa3e9b72070bc2049
cobbland <jacobdensford@proton.me>
2025-06-10 12:19:57 -0400

    Add gameplay logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,81 @@
 import { useState } from 'react';
 import Score from './components/Score.jsx';
 import Card from './components/Card.jsx';
+import logo from './assets/memory-card.png';
 
 export default function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [pokemon, setPokemon] = useState([]);
+  const [clicked, setClicked] = useState([]);
   
   const getPokemon = async () => {
     const twelvePokemon = [];
     while (twelvePokemon.length < 12) {
-      const pokemonNum = Math.floor(Math.random() * 152) + 1;
+      const pokemonNum = Math.floor(Math.random() * 252) + 1;
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNum}`);
       const result = await response.json();
       const poke = {
         name: result.name,
         img: result.sprites.front_default,
+        cry: result.cries.legacy,
       };
-      // Todo: Check for and discard duplicates.
-      twelvePokemon.push(poke);
+      let isDouble = false;
+      for (const object of twelvePokemon) {
+        if (object.name == poke.name) {
+          isDouble = true;
+        }
+      }
+      if (!isDouble) {
+        twelvePokemon.push(poke);
+      }
     }
     setPokemon(twelvePokemon);
-    console.log(twelvePokemon);
   }
+
+  function handleClick(target) {
+    if (!clicked.includes(target)) {
+      setClicked([...clicked, target]);
+      const newScore = score + 1;
+      const newPoke = shuffleArray(pokemon);
+      setPokemon(newPoke);
+      setScore(newScore);
+      if (clicked.length === 11) {
+        console.log("You win!");
+      }
+    } else {
+      setBestScore(score);
+      setClicked([]);
+      setScore(0);
+      setPokemon([]);
+    }
+  }
+
+  function shuffleArray(oldArr) {
+    const array = oldArr;
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  } 
 
   return (
     <>
       <h1>Memory Card</h1>
-      <p className="instructions">Click on each card only once!</p>
+      <p className="instructions">Click on each Pokémon card only once to catch 'em all and win!</p>
       <Score score={score} bestScore = {bestScore} />
-      <Card />
+      <div className="cards">
+        {pokemon.map((each) => (
+          <Card 
+            key={each.name} 
+            name={each.name} 
+            image={each.img} 
+            cry={each.cry}
+            onClick={handleClick} 
+          />
+        ))}
+      </div>
       <button onClick={getPokemon}>Start</button>
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,15 @@
-export default function Card() {
+export default function Card({ name, image, cry, onClick}) {
+    const pokeCry = new Audio(cry);
+
+    function handleClick(target) {
+        // pokeCry.play();
+        onClick(target);
+    }
+
     return (
-        <div className="card">A CARD</div>
+        <div className="card" onClick={() => handleClick(name)}>
+            <img src={image} alt="" />
+            <h2>{name}</h2>
+        </div>
     )
 }
